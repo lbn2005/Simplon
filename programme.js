db17be02579de79f6f1aca43ba1b85c333544a5c
lbn2005 <loubnahmz2001@gmail.com>
2025-08-11 00:41:24 +0100

    Second

--- programme.js
+++ programme.js
@@ -1,12 +1,16 @@
+
+
 const apiURL = "http://localhost:3000/programmes";
     const programmesList = document.getElementById("list");
     const form = document.getElementById("form");
     const searchInput = document.getElementById("recherche");
+    const cleanInput = document.getElementById("supprimer");
+
     let programmesData = [];
     function afficherProgrammes(data) {
-      List.innerHTML = "";
+      programmesList.innerHTML = "";
       if(data.length === 0){
-        programmesList.innerHTML = "<p>Aucun programme trouvé.</p>";
+        programmesList.innerHTML = " ";
         return;
       }
     data.forEach(p => {
@@ -17,6 +21,7 @@ const apiURL = "http://localhost:3000/programmes";
           <p>${p.description}</p>
           <p><strong>Durée:</strong> ${p.duree}</p>
           <p><strong>Compétences:</strong> ${p.competences.join(", ")}</p>
+          <button id="supprimer" oncklic="supprimerprogramme('${p.id}')"> Supprimer</button>
         `;
          programmesList.appendChild(div);
       });
@@ -53,4 +58,13 @@ form.addEventListener("submit", e => {
       );
       afficherProgrammes(filtered);
     });
+function supprimerprogramme(id){
+  axios.delete(`http://localhost:3000/programmes/${id}`)
+  .then(() => {
+          console.log(id)
+          lProgrammes()
+          })
+          .catch(error => console.error(error));
+
+}
   lProgrammes();
